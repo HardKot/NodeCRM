c7be43dcac8a0d3849c1259ae4bda7770bba3444
Batiukhno Nikita <nikita@batyukhno-dev.ru>
2026-07-05 10:57:01 +0500

    Что-то поменялось

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,13 @@
 import js from '@eslint/js';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
+import unicorn from 'eslint-plugin-unicorn';
 import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
   {
     files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
-    plugins: { js },
+    plugins: { js, unicorn },
     extends: ['js/recommended'],
     languageOptions: { globals: globals.node },
     rules: {
@@ -20,12 +21,15 @@ export default defineConfig([
       '@typescript-eslint/no-explicit-any': 'error',
       '@typescript-eslint/no-non-null-assertion': 'error',
       '@typescript-eslint/explicit-function-return-type': 'error',
+      'unicorn/prefer-node-protocol': 'error',
+      'unicorn/no-named-default': 'error',
 
       strict: 'warn',
       'no-undef': 'warn',
       'func-name-matching': 'warn',
       'func-names': 'warn',
       'max-nested-callbacks': ['warn', 3],
+      'unicorn/filename-case': ['warn', { case: 'camelCase' }]
     },
   },
   tseslint.configs.recommended,
